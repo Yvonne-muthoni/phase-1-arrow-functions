a7fdea5ae376e541f95a3b4d34fb35c81d88fade
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-03-26 21:42:38 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (a,b)=>{
+console.log(a/b);
+return a/b;
+}
+
+divide(2000,100);
+
+const square = (x) =>{
+    console.log(x * 2);
+    return x*2;
+}
+square(2)
+
+ const add =( a,b)=>{
+    console.log(a+b);
+    return a+b;
+ }
+ add(3,4)
